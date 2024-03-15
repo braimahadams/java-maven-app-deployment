1a9879aacc05ee84506bc5456dbd1f250d0e1d36
Briamah Adams <braimahadams01@gmail.com>
2024-03-15 14:30:59 +0100

    groovy scipt

--- script.groovy
+++ script.groovy
@@ -1,18 +1,19 @@
-def buildJar {} (
+def buildJar () {
     echo "building the application..."      
     sh 'mvn package'
-)
+}
 
-def buildAndPushDockerImage {} (
+def buildAndPushDockerImage () {
     echo "building the docker image..."    
     withCredentials ([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh "docker build -t braimahadams/java-maven-app:jma-3.0 ."
         sh "echo ${PASSWORD} | docker login -u ${USERNAME} --password-stdin"
-        sh "docker push braimahadams/java-maven-app:jma-3.0 "
-)
+        sh "docker push braimahadams/java-maven-app:jma-3.0 " 
+        }
+}
 
-def deployApp {} (
+def deployApp () {
      echo "deploying the application..."    
-)
+}
 
 return this

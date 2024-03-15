f363a124a6a9a94aed1dd2d7d75acf84520236d1
Briamah Adams <braimahadams01@gmail.com>
2024-03-15 05:02:32 +0100

    update

--- Jenkinsfile
+++ Jenkinsfile
@@ -39,7 +39,6 @@ pipeline {
         stage('deploy') {
             steps {
                 script {
-                    gv.message()
                     gv.deployApp()
                 }
             }
--- script.groovy
+++ script.groovy
@@ -13,15 +13,6 @@ def deployApp() {
     echo "deploying version ${params.VERSION} of the app"
 }
 
-def message() {
-    input {
-        message "select environment to deploy to"
-        ok "Deploy"
-        parameters {
-            choice(name: 'ENVIRONMENT', choices: ['Development', 'Staging', 'Production'], description: 'Select the environment')
-        }
-    }
-}
 
 return this
 
